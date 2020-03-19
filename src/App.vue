ae4284aa82a7c7409c407d825fde018f43a8faf0
szerholder <lisadesh1@gmail.com>
2020-03-19 10:44:01 +0100

    wersja beta do neta

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,49 @@
   <div id="app" class="container">
 
 
+    <h1>Easy Rentier</h1>
+    <ul>
+      <li><a href="#" target="_blank" rel="noopener">Kamery</a></li>
+      <li><a href="#" target="_blank" rel="noopener">Specjaliści</a></li>
+      <li><a href="#" target="_blank" rel="noopener">Szkolenia</a></li>
+    </ul>
 
 
-
-
-
-    <init class="fixed-bottom"/>
     
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">Wybierz kamerę:</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>FLIR E4 WIFI KAMERA TERMOWIZYJNA 80X60</option>
+      <option>FLIR C2 KAMERA TERMOWIZYJNA 80X60</option>
+      <option>SONEL KT-320 KAMERA TERMOWIZYJNA</option>
+      <option>TESTO 855 KAMERA TERMOWIZYJNA </option>
+      <option>FLUKE TIS45 KAMERA TERMOWIZYJNA 160X120</option>
+    </select>
+    </div>
+
+
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">Wybierz miasto:</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Kraków</option>
+      <option>Częstochowa</option>
+      <option>Warszawa</option>
+      <option>Gdańsk </option>
+    </select>
+    </div>
+
+
+    <div class="form-group">
+    <label for="exampleFormControlTextarea1">Dodatkowe informacje</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+
+
+    <div class="d-flex flex-row-reverse">
+      <div><button type="button" class="btn btn-primary">Złóż zlecenie</button></div>
+    </div>
+
+
   </div>
 </template>
 
@@ -16,19 +52,15 @@
 
 
 
-
-
 <script>
-import init from './components/init.vue'
+
 
 export default {
   name: 'App',
   components: {
-    init
   },
   data(){
     return{
-      lista: ["pierwszy","drugi","trzeci"]
 
     }
 
@@ -46,10 +78,17 @@ export default {
   margin-top: 60px;
 }
 
+.container {
+  max-width: 800px;
+}
 
-.kafelkiodlewej{
-  display: flex;
-  flex-direction: row-reverse;
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
+ul {
+  list-style-type: none;
+  padding: 0;
 }
 </style>
