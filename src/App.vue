b868fd31110e35f155fb6454c1fd53a3f8cab43a
szerholder <lisadesh1@gmail.com>
2020-03-19 02:14:57 +0100

    pierwsza wlasna class, przyklad uzycia flexbox i bootstrapowego contantera, row i col

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
   <div id="app">
     
+    
+    <div class="container kafelkiodlewej">
+      <div> A </div>
+      <div> B </div>
+      <div> C </div>
+      <div> D 
+        <br>D
+        <br>D
+      </div>
+    </div>
+
+    <br>
+    <br>
+    <br>
+
+    <b-containter>
+      <b-row>
+        <b-col> A </b-col>
+        <b-col> B </b-col>
+        <b-col> C </b-col>
+        <b-col> D </b-col>
+      </b-row>
+   </b-containter>
 
 
 
