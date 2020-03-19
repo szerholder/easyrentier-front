72ab31d4be15c71b89472d26d2fa029a9c3b189a
szerholder <lisadesh1@gmail.com>
2020-03-19 08:53:58 +0100

    wyrzucenie dotychczasowej pracy do templejta init, usuniecie helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,14 @@
 <template>
   <div id="app" class="container">
+
+
+
+
+
+
+
+    <init class="fixed-bottom"/>
     
-    
-    <div class="container kafelkiodlewej bg-secondary">
-      <div> A </div>
-      <div> B </div>
-      <div> C </div>
-      <div> D 
-        <br>D
-        <br>D
-      </div>
-    </div>
-
-    <br>
-
-    <b-containter class="bg-primary">
-      <b-row class="bg-warning">
-        <b-col class="bg-primary"> A </b-col>
-        <b-col> B </b-col>
-        <b-col> C </b-col>
-        <b-col> D </b-col>
-      </b-row>
-   </b-containter>
-
-    <br>
-    <br>
-    <br>
-
-   <b-container class="bg-success">
-     <ul>
-      <li v-for="item in lista" v-bind:key="item">{{item}}</li>
-     </ul>
-   </b-container>
-   
-     
-  
-
-
   </div>
 </template>
 
@@ -47,12 +19,12 @@
 
 
 <script>
-
+import init from './components/init.vue'
 
 export default {
   name: 'App',
   components: {
-
+    init
   },
   data(){
     return{
---
+++ src/components/init.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div class="container kafelkiodlewej bg-secondary">
+        <div> A </div>
+        <div> B </div>
+        <div> C </div>
+        <div> D 
+            <br>D
+            <br>D
+        </div>
+        </div>
+
+        <br>
+
+        <b-containter class="bg-primary">
+        <b-row class="bg-warning">
+            <b-col class="bg-primary"> A </b-col>
+            <b-col> B </b-col>
+            <b-col> C </b-col>
+            <b-col> D </b-col>
+        </b-row>
+        </b-containter>
+
+        <br>
+
+
+        <b-container class="bg-success">
+            <ul>
+                <li v-for="item in lista" v-bind:key="item">{{item}}</li>
+            </ul>
+        </b-container>
+
+    </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'init',
+  data(){
+    return{
+      lista: ["pierwszy","drugi","trzeci"]
+
+    }
+
+  }
+}
+</script>
+
