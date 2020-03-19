4a2d83c8215a153e387e5e294ff70154dfb1dbbc
szerholder <lisadesh1@gmail.com>
2020-03-19 02:13:14 +0100

    init

--- src/App_old.vue
+++ src/App_old.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
-    
-
-
-
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -14,12 +12,12 @@
 
 
 <script>
-
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-
+    HelloWorld
   }
 }
 </script>
@@ -32,12 +30,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-
-.kafelkiodlewej{
-  display: flex;
-  flex-direction: row-reverse;
-
 }
 </style>
