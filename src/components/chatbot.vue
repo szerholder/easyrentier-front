b6e354d36af6e1e58fb3d3b159ec1cb9eeb3cd04
szerholder <lisadesh1@gmail.com>
2020-03-20 20:50:49 +0100

    chatbox 1.1

--- src/components/chatbot.vue
+++ src/components/chatbot.vue
@@ -1,28 +1,34 @@
 <template>
 <div>
 
-    <div v-if="chatboxview==1" class="chatbox">
-        <div v-if="start==1">
+    <div v-if="chatboxview==1" class="chatbox right">
+        <div class="bg-primary bluetitle d-flex justify-content-end">
+            <button v-on:click="showChat">chat</button>
+        </div>
+
+        <div v-if="start==1" class="chatbox">
             <!-- <div><input v-model="zmienna"><br></div>
             <div>{{zmienna}}</div> -->
             <div><button class="btn btn-secondary">Dzień dobry! Jak mogę Ci pomóc?</button></div>
+            <br>
             <div><button class="btn btn-secondary" v-on:click="showCameraQuestions">Chcę kamerę.</button></div>
             <div><button class="btn btn-secondary">Chcę badanie.</button></div>
             <div><button class="btn btn-secondary">Chcę zlecenie.</button></div>
             
         </div>
 
-        <div v-if="zmienna==1">
+        <div v-if="zmienna==1" class="chatbox">
             <div><button class="btn btn-secondary">W jakim celu?</button></div>
-            <div><button class="btn btn-secondary">Chcę sie pobawić.</button></div>
-            <div><button class="btn btn-secondary">Chcę sprawdzić maszynę.</button></div>
+            <br>
             <div><button class="btn btn-secondary">Chcę sprawdzić okno.</button></div>
-            <div><button class="btn btn-secondary">Chcę zostać sie przeszkolić.</button></div>
+            <div><button class="btn btn-secondary">Chcę sprawdzić maszynę.</button></div>
+            <div><button class="btn btn-secondary">Chcę sie przeszkolić.</button></div>
+            <div><button class="btn btn-secondary">Nie interesuj się.</button></div>
             <div><button class="btn btn-secondary" v-on:click="showStartQuestions">Wróć.</button></div>
 
         </div>
 
-        <div v-if="zmienna==2">
+        <div v-if="zmienna==2" class="chatbox">
             <div><button class="btn btn-secondary">Dzień dobry! Jak mogę Ci pomóc?</button></div>
             <div><button class="btn btn-secondary">Chcę kamerę.</button></div>
             <div><button class="btn btn-secondary">Chcę badanie.</button></div>
@@ -30,9 +36,9 @@
             
         </div>
     </div>
-
-    <button v-on:click="showChat">chat</button>
-
+    <div class="right">
+    <button  v-on:click="showChat">chat</button>
+    </div>
 
 
 
@@ -75,14 +81,22 @@ export default {
 
 <style>
 
-.chatbox{
-background-color:darkgoldenrod;
-z-index: 2;
-height: 200px;
-width: 200x;
+.bluetitle{
+height: 30px;
+}
+
+
+.right{
 right: 0;
-bottom: 0;
 position: absolute;
+top:100px;
+}
+
+.chatbox{
+background-color: whitesmoke;
+z-index: 2;
+height: 250px;
+width: 300px;
 }
 
 
